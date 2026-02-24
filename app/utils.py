e4137bb2838b1708f85cb70e6f74ac73929278f8
lowhumlee <100022344+lowhumlee@users.noreply.github.com>
2026-02-24 09:57:17 +0200

    Delete app directory

--- app/utils.py
+++
@@ -1,9 +0,0 @@
-import re
-from unidecode import unidecode
-
-def normalize_name(name: str) -> str:
-    name = unidecode(name or "")
-    name = name.lower()
-    name = re.sub(r"[^a-z ]", " ", name)
-    name = re.sub(r"\s+", " ", name).strip()
-    return name
